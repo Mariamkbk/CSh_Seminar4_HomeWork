25e6fdcc94eb00f14712d88f9a7ad65928f105d9
Masulka <kuzmenkomkbk@gmail.com>
2022-12-03 22:52:45 +0300

    task 26 (opt) finished

--- Sem4_26_HARD(opt)/Program.cs
+++ Sem4_26_HARD(opt)/Program.cs
@@ -1,16 +1,34 @@
-﻿// Напишите программу, которая принимает на вход целое или дробное 
+﻿// Напишите программу, которая принимает на вход целое или дробное
 // число и выдаёт количество цифр в числе.
 // 452 -> 3
 // 82 -> 2
 // 9,012 ->4
 
-Console.WriteLine("Введите целое или дробное число");
-double num = Convert.ToDouble(Console.ReadLine());
-int count = 0;
-while(num != Math.Round(num)) num=num*10;
-while (num > 0) 
+decimal NumCount(decimal num)
 {
-    num=Math.Round(num/10);
+int count = 0;
+if (num < 0) num = -num;
+if (num==0) count = 1;
+while (Math.Floor(num) == 0 && num!=0)
+{
+    num *= 10;
     count++;
 }
-Console.WriteLine($"В заданном числе {count} цифр");
+while (num != Math.Floor(num)) num *= 10;
+while (num > 0)
+{
+    num = Math.Floor(num / 10);
+    count++;
+}
+return count;
+}
+try
+{
+Console.WriteLine("Введите целое или дробное число");
+decimal num = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"Количество цифр числе {num} равно {NumCount(num)}");
+}
+catch
+{
+    Console.WriteLine("Введены некорректные данные");
+}
