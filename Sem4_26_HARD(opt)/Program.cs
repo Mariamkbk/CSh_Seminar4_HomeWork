b3349faa631494fed94277c3fdbcdb6920dd3a24
Masulka <kuzmenkomkbk@gmail.com>
2022-12-03 20:24:41 +0300

    task 26 (opt) in progress

--- Sem4_26_HARD(opt)/Program.cs
+++ Sem4_26_HARD(opt)/Program.cs
@@ -4,3 +4,13 @@
 // 82 -> 2
 // 9,012 ->4
 
+Console.WriteLine("Введите целое или дробное число");
+double num = Convert.ToDouble(Console.ReadLine());
+int count = 0;
+while(num != Math.Round(num)) num=num*10;
+while (num > 0) 
+{
+    num=Math.Round(num/10);
+    count++;
+}
+Console.WriteLine($"В заданном числе {count} цифр");
