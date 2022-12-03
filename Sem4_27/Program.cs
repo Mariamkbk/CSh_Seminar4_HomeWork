1f24925dd30a3f4acf6a8cb5005c54a0c8b4b4e3
Masulka <kuzmenkomkbk@gmail.com>
2022-12-03 19:06:23 +0300

    task 27 finished

--- Sem4_27/Program.cs
+++ Sem4_27/Program.cs
@@ -1,6 +1,26 @@
-﻿// Напишите программу, которая принимает на вход число 
+﻿// Напишите программу, которая принимает на вход число
 // и выдаёт сумму цифр в числе. Через строку решать нельзя.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
 
+int NumSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+try
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Сумма цифр в числе {num} равна {NumSum(num)}");
+}
+catch
+{
+    Console.WriteLine("Введены некорректные данные");
+}
