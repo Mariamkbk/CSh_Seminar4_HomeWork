85927e7c235cd4fa052222d926d5ba17fc35c10c
Masulka <kuzmenkomkbk@gmail.com>
2022-12-04 13:32:36 +0300

    All saved

--- Sem4_30_HARD(opt)/Program.cs
+++ Sem4_30_HARD(opt)/Program.cs
@@ -3,38 +3,35 @@
 // Далее надо посчитать количество нулей и единиц,
 // если единиц больше чем нулей то вывести
 // TRUE на экран, иначе вывести False.
-
 void FillArray(int[] SomeArray)
 {
-    for (int i = 0; i < 8; i++)
-    {
-        SomeArray[i] = new Random().Next(0, 2);
-    }
+    for (int i = 0; i < 8; i++) SomeArray[i] = new Random().Next(0, 2);
 }
-
 void PrintArray(int[] RandomArray)
 {
+    for (int i = 0; i < 8; i++) Console.Write($"{RandomArray[i]} ");
+}
+string NullOneCount(int[] AnothArray)
+{
+    string res = String.Empty;
     int CountOne = 0;
     int CountNull = 0;
-    for (int i = 0; i < 8; i++) Console.Write($"{RandomArray[i]} ");
-for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++) 
     {
-        while (RandomArray[i] == 1) CountOne++;
+      if (AnothArray[i] == 1) CountOne++;
+      else CountNull++;
     }
-    for (int i = 0; i < 8; i++)
-    {
-        while (RandomArray[i] == 0) CountNull++;
-    }
-    if (CountOne > CountNull) Console.Write($"{RandomArray[i]} ");
-    else if (CountOne < CountNull) Console.Write($"{RandomArray[i]} ");
-    else Console.Write($" ");
-
+    if (CountOne > CountNull) res = res + "TRUE";
+    else if (CountOne < CountNull) res = res + "FALSE";
+    else res = res + "EQUAL";
+    return res;
 }
 try
 {
     int[] array = new int[8];
     FillArray(array);
     PrintArray(array);
+    Console.WriteLine();
     Console.WriteLine(NullOneCount(array));
 }
 catch
